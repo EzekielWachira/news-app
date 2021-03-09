620fab9e0925586e64bbcd3411587ffa59547af2
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-09 12:03:52 +0300

    :tada:initial commmit

---
+++ app/src/main/java/com/ezzy/newsapp/ui/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.ezzy.newsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ezzy.newsapp.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
