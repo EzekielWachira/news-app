107c4ced55261f2cb6492c4b0118469579879bd3
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-10 19:03:20 +0300

    :lipstick: change app color

---
+++ app/src/main/java/com/ezzy/newsapp/util/RecyclerViewItemDecorator.kt
@@ -0,0 +1,12 @@
+//package com.ezzy.newsapp.util
+//
+//import android.content.Context
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class RecyclerViewItemDecorator(
+//        context: Context,
+//        val paddingLeft: Int,
+//        val paddingRight: Int
+//) : RecyclerView.ItemDecoration() {
+//
+//}
